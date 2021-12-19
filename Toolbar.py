1b05b70d181ed75b0ba39a40f3af94177a1f5669
Teaalin <Teaalin@users.noreply.github.com>
2021-12-19 14:29:39 +1000

    improvements
    added json parsing of config so it is easier to read. fixed the buttons not stretching accordingly

--- Toolbar.py
+++ Toolbar.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import re
-import tkinter
 from functools import partial
 from pyautogui import hotkey
 from tkinter import ttk
-
-import random
+from tkinter import *
+import json
+import re
 
 # Customise here instead of having config files for a single portable Toolbar.py file
 # ======================================
@@ -17,27 +16,26 @@ background_colour = "#282e33"
 button_colour = "#33393f"
 text_colour = "white"
 
-default_config = 'undo "ctrlleft","z"    redo "ctrlleft","shift","z"\n'\
-                 'copy "ctrlleft","c"    paste "ctrlleft","v"\n'\
-                 'transform "ctrlleft","t"\n'\
-                 'deselect "ctrlleft","shift","a"'
+default_buttons = [
+    "UNDO : ctrlleft,z      REDO : ctrlleft,shift,z",
+    "COPY : ctrlleft,c      PASTE : ctrlleft,v",
+    "TRANSFORM : ctrlleft,t",
+    "DESELECT : ctrlleft,shift,a"
+]
 
-button_border = 1
-# Padding on the left and right of the button
-PADX = 10
-# Padding on the top and buttom of the button
-PADY = 8
-# Stickies the buttons to left and right sides, change to "" if you don't want stretched buttons
-stick = "EW" # set with variations of "NSEW" or ""
+# Button size padding (positvie # only)
+button_padding = [3, 8]
+# Between buttons (positvie # only)
+button_spacing = [2, 2]
+
 # ======================================
 # End of user customisation
 
-frames = []
-borders = []
-buttons = []
+X = 0
+Y = 1
 
 # Initialize and configure the window
-root = tkinter.Tk()
+root = Tk()
 root.title("Toolbar")
 root.resizable(0,0)
 root.attributes("-topmost", True)
@@ -46,87 +44,76 @@ style = ttk.Style()
 
 # Functions
 def setup():
-    settings = []
+    settings = {}
     buttons = []
 
     # Read the user config if it exists
     try:
-        file = open("toolbar_config.txt", "r")
+        file = open("toolbar_config.json", "r")
         lines = file.read()
-        settings = re.findall(r"settings:{\s*(\w.*?)}", lines, re.DOTALL)[0].replace(" ", "")
-        buttons = re.findall(r"buttons:{\s*(\w.*?)}", lines, re.DOTALL)[0]
+        settings = json.loads(lines)
 
-        load_settings(settings)
+        if "buttons" in settings:
+            buttons = settings["buttons"]
+            settings.pop("buttons")
         
+        load_settings(settings)
         file.close()
     except IOError:
         print("No config found, using default settings")
-        buttons = default_config.split("\n")
     except EOFError:
         print("There are no contents in the file")
-        buttons = default_config.split("\n")
     except:
         print("ERR: configuration invalid, using defaults")
 
     if buttons == []:
-        buttons = default_config.split("\n")
+        buttons = default_buttons
 
     load_buttons(buttons)
+    apply_settings()
     
-    # Apply configuration
+
+def load_settings(settings):
+    secure_varibles = ["alt_tab", "highlight_colour", "background_colour", "button_colour", "text_colour", "button_padding", "button_spacing"]
+
+    for variable in settings.keys():
+        if variable in secure_varibles:
+            # yes, I know globals are bad and I should feel bad
+            globals()[variable] = settings[variable]
+        else:
+            print("WARN config: \"" + variable + "\" is unchangable or is invalid. Skipping")
+
+def apply_settings():
     global style
     global root
     root.configure(bg=background_colour)
-    style.configure("TButton", background=button_colour, foreground=text_colour, borderwidth=0, padding=(PADX, PADY))
-    #style.configure("TFrame", background="black")
+    style.configure("TFrame", background=background_colour)
+    style.configure("TButton", background=button_colour, foreground=text_colour, borderwidth=0, padding=(button_padding[X], button_padding[Y]))
     style.map('TButton', background=[('active', highlight_colour)])
     root.mainloop()
 
-def load_settings(lines):
-        secure_varibles = ["alt_tab", "highlight_colour", "background_colour", "button_colour", "text_colour", "button_border", "PADX", "PADY", "stick"]
-        settings = re.findall(r"(\w+) *= *([\w#'\",]+)", lines, re.DOTALL)
-        print(settings)
-        for line in settings:
-            #for variable, value in line: # 2 is > 2 apparently 
-            variable = line[0]
-            value = line[1]
-            if not variable in secure_varibles:
-                print("WARN config: \"" + variable + "\" is unchangable or is invalid. Skipping")
-            else:
-                # yes, I know globals are bad and I should feel bad
-                exec(str("globals()['" + variable + "']=" + value))
-
-def load_buttons(lines):
-    row = 1; col = 1; cols = 1
+def load_buttons(config):
     global root
 
-    for line in lines.split('\n'):
-        button_config = re.findall("\w+ *: *[\w,']+", line)
-
+    for line in config:
         new_frame = ttk.Frame(root)
-        new_frame.grid(row=row, sticky=stick)
-        frames.append(new_frame)
+        new_frame.pack(fill="both", expand=True)
+
+        button_config = re.findall("[^\s:]+ *: *[^\s]+", line) #"\w+ *: *[\w,']+"
 
         for button in button_config:
-            # Create a frame for the button so that it can have a "border"
-            border_frame = ttk.Frame(new_frame, borderwidth=button_border)
-            border_frame.grid(row=row, column=col, sticky=stick)
-            borders.append(border_frame)
-
-            # Create the button
             display_string, button_hotkey = button.replace(" ", "").split(":")
-            
-            new_button = ttk.Button(border_frame, text=display_string, command=partial(press_hotkey, button_hotkey))
-            new_button.grid(sticky="NSEW")
-            buttons.append(new_button)
-            col += 1
 
-        row += 1
-        col = 1
+            # Create a button
+            ttk.Button(new_frame, text=display_string, command=partial(press_hotkey, button_hotkey)
+                      ).pack(side=LEFT, fill="both", expand=True, padx=button_spacing[X], pady=button_spacing[Y])
 
 def press_hotkey(keys):
-    exec( str("hotkey("+ alt_tab +")") )
-    print( str("hotkey("+ keys +")") )
-    exec( str("hotkey("+ keys +")") )
+    exec( str("hotkey("+ format_hotkey(alt_tab) +")") )
+    exec( str("hotkey("+ format_hotkey(keys) +")") )
+
+def format_hotkey(hotkeys):
+    temp = "'" + hotkeys.replace(",", "','") + "'"
+    return temp
 
 setup()
